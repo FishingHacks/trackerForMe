f50bc6923294278ff22433d9d60d21fb016d0707
FishingHacks <thejominingcraft@gmail.com>
2022-02-16 14:18:39 +0100

    Fixed error where the bot send update 3 times   Changes to be committed: 	modified:   config.js 	modified:   index.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Your discord id, you bot's token and the notification channel
 /**
  * @exports require("./type.d.ts").config configuration (.id, .token, .channel)*/
-module.exports={id:"",token:"",channel: ""};
+module.exports={id:"",token:"",channel:""}
